946cc7d0deccffc2ab398179622c91887d86674d
mayazhao-myz <3065626523@qq.com>
2021-04-19 19:50:15 +0800

    前端第一次

---
+++ src/common/config.js
@@ -0,0 +1,5 @@
+let Config = {};
+
+Config.serverUrl = "http://localhost:8082";
+
+export default Config;
---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="home-wrap">
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-fa fa-home"></i> 首页
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+
+        <div class="main-wrap">
+            <el-container class="main-container">
+                <div class="introduction-container">
+                    <p style="font-size: 60px;text-align: center;width: 100%;height: 45px;line-height: 45px;display: block">
+                        欢迎来到海滨作业管理系统！
+                    </p>
+                </div>
+            </el-container>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    .home-wrap {
+        width: 100%;
+        height: 100%;
+    }
+
+    .main-wrap {
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+        background-color: #ffffff;
+        border: 1px solid #dddddd;
+        border-radius: 5px;
+    }
+
+    .main-container {
+        width: 90%;
+        height: 100%;
+        text-align: justify-all;
+        margin: auto;
+    }
+
+    .introduction-container {
+        font-size: 16px;
+    }
+</style>
